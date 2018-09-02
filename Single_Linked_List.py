fe5c097c6c38b52115d603100a31e0eed1b67a83
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-09-03 00:40:53 +0530

    Singly Linked List

--- Single_Linked_List.py
+++ Single_Linked_List.py
@@ -11,7 +11,7 @@ class linked_list:
 
     def append(self, data):
         new_node = node(data)   # New Node Data
-        cur = self.head         # initialising cursor to head node
+        cur = self.head         # initialising current node to head node
         while cur.next != None: # finding last node
             cur = cur.next
             
@@ -33,6 +33,36 @@ class linked_list:
             elements.append(cur.data)
         return elements
 
+
+#--- main ---
+
 mylist = linked_list()
-mylist.display()
+while True:
+       print ("SINGLY LINKED LIST OPERATIONS")
+       print ("1. Add")
+       print ("2. Length")
+       print ("3. Traverse")
+       print ("4. Exit")
+       ch=int(input("\nEnter your choice :: "))
+       if ch==1:
+              data=input("Enter Data :")
+              mylist.append(data)
+              print("*** New Node Added ***")
+       elif ch==2:
+              length = mylist.length()
+              print ("*** Length =  ",length," ***")
+       elif ch==3:
+              nodes=mylist.display()
+              print("Linked List - > " , end="")
+              for node in nodes:
+                  print(node,end=", ")
+       elif ch==4:
+              break
+       else:
+              print ("\tInvalid Choice!!!")
+       print("\n","="*50,"\n")
+       
+
+
+
     
