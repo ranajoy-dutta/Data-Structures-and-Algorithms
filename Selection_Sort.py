0af0738444beb70ad1d8d1c70e21bc26b5aa87ac
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-09-04 23:43:30 +0530

    Selection Sort
    Selection Sort - Sorting Algorithm in Python

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,13 +1,15 @@
 def selectionSort(arr):
     for i in range(len(arr)):
-        min_pos = i
-        for j in range(i,len(arr)):
+        min_pos = i                         # assuming i as the minimum element
+        for j in range(i,len(arr)):         # finding the minimum element after i
             if arr[min_pos] > arr[j]:
                 min_pos = j
-        if min_pos != i:
+                
+        if min_pos != i:                    # swapping the minimum element with i position
             temp = arr[i]
             arr[i] = arr[min_pos]
             arr[min_pos] = temp
+
     return arr
 
 # --- main ---
