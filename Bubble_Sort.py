2961b7fc95a8f3322d679c637614c819fa61b35e
Ranajoy Dutta <ranajoydutta7@gmail.com>
2022-01-07 23:13:16 +0530

    reduced iterations

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,13 +1,15 @@
-def bubbleSort(arr):
+def bubble_sort(arr):
     for i in range(len(arr)):
-        for j in range(i,len(arr)):
-            if arr[i] > arr[j]:
-                temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
+        swapped = False
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if swapped==False:          # exit if array is already sorted
+            break
     return arr
 
 # --- main ---
 array = list(map(int, input("Enter space separated numbers : ").split()))
-sorted_array = bubbleSort(array)
+sorted_array = bubble_sort(array)
 print("Sorted Array : ",sorted_array)
